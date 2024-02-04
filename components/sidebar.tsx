c9e1fa0304159d7803ddf0b36cecaceab44fb7bd
Ibrahim Jhabuawala <ijhabuawala@gmail.com>
2024-02-04 10:30:47 -0500

    adding scrolling for playlist

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -31,46 +31,95 @@ const navMenu = [
     },
     {
         name: 'Your Library',
-        icon: MdLibraryMusic,
-        route: '/library',
+        icon: MdFavorite,
+        route: '/favorites',
     },
 ]
-const Sidebar=() => {
-    return (
-        <Box 
-            width="100%" 
-            height="calc(100vh - 100px)" 
-            bg="black" 
-            paddingX="5px"
-            color="gray"
-        >
-            <Box paddingY="20px">
-                <Box width="120px" marginBottom="20px" paddingX="20px" >
-                <NextImage src="/logo.png" height={60} width={120} alt="Logo" />
-                </Box>
-                <Box marginBottom="20px">
-                    <List spacing={2}>
-                        {navMenu.map(menu => (
-                            <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
-                                <LinkBox>
-                                    <NextLink href={menu.route} passHref>
-                                        <LinkOverlay>
-                                            <ListIcon 
-                                                as={menu.icon}
-                                                color ="white" 
-                                                marginRight="20px"
-                                                />
-                                                {menu.name}
-                                        </LinkOverlay>
-                                    </NextLink>
-                                </LinkBox>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Box>
-            </Box>
-        </Box>
-    )
-}
 
-export default Sidebar
+const musicMenu = [
+    {
+        name: 'Create Playlist',
+        icon: MdPlaylistAdd,
+        route: '/',
+    },
+    {
+        name: 'Favorites',
+        icon: MdHome,
+        route: '/',
+    }
+
+]
+const playlist = new Array(30).fill(1).map((_, i) => `Playlist ${i+1}`);
+const Sidebar = () => {
+    return (
+      <Box
+        width="100%"
+        height="calc(100vh - 100px)"
+        bg="black"
+        paddingX="5px"
+        color="gray"
+      >
+        <Box paddingY="20px" height="100%">
+          <Box width="120px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/logo.png" height={60} width={120} alt="Logo" />
+          </Box>
+          <Box marginBottom="20px">
+            <List spacing={2}>
+              {navMenu.map((menu) => (
+                <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                  <LinkBox>
+                    <NextLink href={menu.route} passHref>
+                      <LinkOverlay>
+                        <ListIcon
+                          as={menu.icon}
+                          color="white"
+                          marginRight="20px"
+                        />
+                        {menu.name}
+                      </LinkOverlay>
+                    </NextLink>
+                  </LinkBox>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+          <Box marginTop="20px">
+            <List spacing={2}>
+              {musicMenu.map((menu) => (
+                <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                  <LinkBox>
+                    <NextLink href={menu.route} passHref>
+                      <LinkOverlay>
+                        <ListIcon
+                          as={menu.icon}
+                          color="white"
+                          marginRight="20px"
+                        />
+                        {menu.name}
+                      </LinkOverlay>
+                    </NextLink>
+                  </LinkBox>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+          <Divider color="gray.800" />
+          <Box height="66%" overflowY="auto" paddingY="20px" >
+                <List spacing={2}>
+                    {playlist.map((playlistItem, index) => (
+                        <ListItem paddingX="20px" key={index}>
+                            <LinkBox>
+                            <NextLink href="/" passHref>
+                                <LinkOverlay>{playlistItem}</LinkOverlay>
+                            </NextLink>
+                        </LinkBox>
+                    </ListItem>
+                ))}
+            </List>
+          </Box>
+        </Box>
+      </Box>
+    )
+  }
+  
+  export default Sidebar
